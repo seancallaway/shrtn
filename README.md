730cf0c0f70617a88a431c47fc6dd4f83b6c6461
Sean Callaway <Seancallaway@gmail.com>
2020-12-02 13:18:08 -0800

    Add Redirects to Admin (#4)
    * Add Travis CI build badge to README.
    
    * Add Redirect to Django admin.
    
    * Remove trailing slash from URL.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # shrtn
 > A URL-shortening app
 
+[![Build Status][travis-image]][travis-url]
+
 ## Running the Dev Server
 
 ```shell script
@@ -8,3 +10,7 @@
 # Then, run the server:
 DEBUG=1 python manage.py runserver
 ```
+
+<!-- Markdown link & img dfn's -->
+[travis-image]: https://api.travis-ci.com/seancallaway/shrtn.svg?branch=master
+[travis-url]: https://travis-ci.com/seancallaway/shrtn
--- shrtn/urls.py
+++ shrtn/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from redirect.views import DoRedirect, HomePageView, LinkSuccessView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('<slug:slug>', DoRedirect.as_view(), name='redirect'),
     path('', HomePageView.as_view(), name='home'),
     path('success/<slug:slug>', LinkSuccessView.as_view(), name='show-link'),
