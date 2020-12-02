46d42db8a3e32a148850963e54c8c7842841ff5b
Sean Callaway <Seancallaway@gmail.com>
2020-12-02 12:53:04 -0800

    Added creation and success templates. (#3)

--- redirect/views.py
+++ redirect/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import RedirectView
+from django.shortcuts import get_object_or_404, reverse
+from django.views.generic import CreateView, DetailView, RedirectView
 
 from redirect.models import Redirect
 
@@ -11,3 +11,18 @@ class DoRedirect(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         redirect = get_object_or_404(Redirect, slug=kwargs['slug'])
         return redirect.target
+
+
+class HomePageView(CreateView):
+    template_name = 'index.html'
+    model = Redirect
+    fields = ['target']
+
+    def get_success_url(self):
+        return reverse('show-link', kwargs={'slug': self.object.slug})
+
+
+class LinkSuccessView(DetailView):
+    template_name = 'show_link.html'
+    model = Redirect
+    context_object_name = 'my_link'
--- shrtn/urls.py
+++ shrtn/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from redirect.views import DoRedirect
+from redirect.views import DoRedirect, HomePageView, LinkSuccessView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<slug:slug>', DoRedirect.as_view(), name='redirect'),
+    path('', HomePageView.as_view(), name='home'),
+    path('success/<slug:slug>', LinkSuccessView.as_view(), name='show-link'),
 ]
